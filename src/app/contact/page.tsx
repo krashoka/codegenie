93deeebde804ce49124e0f591657033178b6c2ab
ashok-predigle <ashok.kumar@predigle.com>
2025-10-15 18:39:09 +0530

    default pages privacy policy, about, contact added

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Header from "@/components/Header";
+
+export default function Contact() {
+  return (
+    <>
+      <Header />
+      <main className="max-w-3xl mx-auto px-4 py-10">
+        <h1 className="text-3xl font-bold mb-4">Contact Us</h1>
+      </main>
+    </>
+  );
+}
---
+++ src/app/privacy-policy/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Header from "@/components/Header";
+
+export default function PrivacyPolicy() {
+  return (
+    <>
+      <Header />
+      <main className="max-w-3xl mx-auto px-4 py-10">
+        <h1 className="text-3xl font-bold mb-4">Privacy Policy</h1>
+      </main>
+    </>
+  );
+}
